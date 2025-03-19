bba0e364cf78714014f89888ea14cb470d7e569d
Chamijd <chamindumax28@gmail.com>
2025-03-19 21:37:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CHAMA-MD=J5ownboS#DsT8DXksf78RpBouYQuoodGmTglPbm9dMAHMaT-zNw0",
+SESSION_ID: process.env.SESSION_ID || "RoI2SRIR#GFUUVcnqQlvpqj-viaTxzIJOEbY2sVryg_xoVh5cLNY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*JUST NOW YOUR STATUS SEEN BY DEVIL-TECH-MD 🤍👻*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*JUST🥰👻*",
 // set the auto reply massage on status reply  
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
